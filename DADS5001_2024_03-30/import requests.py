c74b0305dc22e11015042c5dfeb6b8654291863d
NattachaiJairak <156494047+NattachaiJairak@users.noreply.github.com>
2024-04-12 14:11:36 +0700

    Delete DADS5001_2024_03-30/import requests.py

--- DADS5001_2024_03-30/import requests.py
+++
@@ -1,8 +0,0 @@
-import requests
-API_URL = "https://api-inference.huggingface.co/models/gpt2"
-headers = {"Authorization": f"Bearer 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"}
-def query(payload):
-    response = requests.post(API_URL, headers=headers, json=payload)
-    return response.json()
-data = query("Can you please let us know more details about your ")
-print(data)
